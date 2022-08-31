158762e7ed8dc1b38c120a4e9f8478755f48d2c7
Michael Golenia <michaelgolenia1@gmail.com>
2022-08-31 14:38:47 -0300

    4-binary_tree_is_leaf.c

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_leaf - check if a node is a lead
+ * @node: is the node to ceck
+ * Return: 1 if is a leaf, oterwhise 0
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (!node)
+		return (0);
+	if (!node->left && !node->right)
+		return (0);
+	return (0);
+}
