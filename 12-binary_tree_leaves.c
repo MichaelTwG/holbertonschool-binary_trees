7d192c0a3167a86d68a0439154025f540fe3b6a1
Michael Golenia <michaelgolenia1@gmail.com>
2022-08-31 22:19:47 -0300

    14-binary_tree_balance.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -9,7 +9,6 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-
 	if (!tree->left && tree->right)
 		return (1);
 	if (tree->left && !tree->right)
---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,51 @@
+#include "binary_trees.h"
+#include "stdio.h"
+/**
+ *  binary_tree_height_aux - funcion that measures the height of a binary tree
+ *  @tree: the binary tree
+ *  Return: depth
+ */
+size_t binary_tree_height_aux(const binary_tree_t *tree)
+{
+	int left_depth = 0, right_depth = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	left_depth = binary_tree_height_aux(tree->left);
+	right_depth = binary_tree_height_aux(tree->right);
+
+	if (left_depth > right_depth)
+		return (left_depth + 1);
+
+	return (right_depth + 1);
+}
+/**
+ * binary_tree_height - funcion that measures the height of a binary tree
+ * @tree: the binary tree
+ * Return: depth
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (binary_tree_height_aux(tree));
+}
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: the binary tree
+ * Return: the balance factor
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int depth_left = 0, depth_right = 0;
+
+	depth_left = binary_tree_height(tree->left);
+	depth_right = binary_tree_height(tree->right);
+
+	depth_left = abs(depth_left);
+	depth_right = abs(depth_right);
+
+	return (depth_left - depth_right);
+}
