934d45110e759bf02ece6614fa3a1e420176983d
MichaelTwG <michaelgolenia1@gmail.com>
2022-09-01 07:17:15 -0700

    files added: 15-binary_tree_is_full.c binary_trees.h

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,17 +1,19 @@
 #include "binary_trees.h"
 /**
- * binary_tree_is_full_aux - check if a binary tree is full
+ * is_full - check if a binary tree is full
  * @tree: is the binary tree
  * Return: is tree is null return 0
  */
-int binary_tree_is_full_aux(const binary_tree_t *tree)
+int is_full(const binary_tree_t *tree)
 {
-	if ((tree->left == NULL && tree->right == NULL))
+	if (!tree)
+		return (1);
+
+	if ((!tree->left && !tree->right))
 		return (1);
 	if ((tree->left) && (tree->right))
 	{
-		return (binary_tree_is_full(tree->left));
-		return (binary_tree_is_full(tree->right));
+		return ((is_full(tree->left)) && (is_full(tree->right)));
 	}
 	return (0);
 }
@@ -24,5 +26,5 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	return (binary_tree_is_full_aux(tree));
+	return (is_full(tree));
 }
